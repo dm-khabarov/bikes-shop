bd360fd671863e529d9cdf1c815218426d92f45b
Dmitriy Khabarov <nixvod@gmail.com>
2014-12-02 16:59:31 +0300

    Add orders slider (jQuery Slick)

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   var thumbnails = $('.thumbnails img'),
       slickSlider = $('.slides');
 
-  $('.slides').slick({
+  slickSlider.slick({
     autoplay: true,
     arrows: false,
     onAfterChange: function() {
@@ -23,5 +23,43 @@ $(document).ready(function(){
   });
 
   thumbnails.eq( slickSlider.slickCurrentSlide() ).css('border-color', '#a1877f');
+
+
+
+  // Orders Slider
+
+  var oSlider = $('.orders-slider ul'),
+      nextBtn = $('.nextBtn');
+
+  oSlider.slick({
+    autoplay: true,
+    infinite: true,
+    slide: 'li',
+    slidesToShow: 3,
+    variableWidth: true
+  });
+
+  var dur = 300; // animate() duration
+
+  nextBtn.hover(function() {
+    nextBtn.animate({
+      right: ['+=7']
+    }, dur);
+    nextBtn.children( "div" ).animate({
+      opacity: [1]
+    }, dur)
+  },
+  function() {
+    nextBtn.animate({
+      right: ['-=7']
+    }, dur);
+    nextBtn.children( "div" ).animate({
+      opacity: [0.57]
+    }, dur)
+  });
+
+  nextBtn.click(function() {
+    oSlider.slickNext();
+  });
 
 });
